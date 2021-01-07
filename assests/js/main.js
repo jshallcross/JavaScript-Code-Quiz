8442bd0ab1d99f9889774314df1a51cec26aaac0
Jon Shallcross <jshallcross1@icloud.com>
2021-01-07 11:06:40 -0500

    score function added/end of quiz function

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,21 +1,17 @@
 
-
-
-
-
-  var startBtnEl = document.getElementById('start-button');
-  var timerSpanEl = document.getElementById('timer-span');
-  var questionsDiv = document.getElementById('questions-div');
-  var wrapper = document.getElementById("wrapper");
-  
+var startBtnEl = document.getElementById('start-button');
+var timerSpanEl = document.getElementById('timer-span');
+var questionsDiv = document.getElementById('questions-div');
+var wrapper = document.getElementById("wrapper");
+var timerEl = document.getElementById("timer"); 
 var questionIndex = 0;
 
 var ulCreate = document.createElement("ul");
-
+var timeLeft = 50;
+var correctAnswers = 0;
 
 
 function countdown(event){
-    var timeLeft = 50;
     event.preventDefault();
 
     var timeInterval = setInterval(function(){
@@ -60,23 +56,53 @@ function checkAnswer(event) {
 
     if (answer.matches("li")){
         var createDiv = document.createElement("div");
-        createDiv.setAttribute("id", "createDive");
+        createDiv.setAttribute("id", "createDiv");
     }
     if (answer.textContent == questions[questionIndex].answer){
+        correctAnswers ++;
         createDiv.textContent = "Correct! The answer is " + questions[questionIndex].answer;
     }
     else {
         createDiv.textContent = "Incorrect! The answer was " + questions[questionIndex].answer;
+        timeLeft = timeLeft -5;
+
     }
 
 questionIndex ++;
 if (questionIndex >= questions.length){
-    createDiv.textContent = "Quiz Over!";
+    alert("Test");
+    quizOver();
+    if (correctAnswers === 1) {
+        createDiv.textContent = "You got " + correctAnswers + " question right!";
+    } else {
+        createDiv.textContent = "You got " + correctAnswers + " questions right!";
+    }
+    
+    
 }
 else {
     showQuestions(questionIndex);
 }
 questionsDiv.appendChild(createDiv);
+}
+
+function quizOver(){
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    timerEl.innerHTML = "";
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "Quiz Over";
+    questionsDiv.appendChild(createH1);
+    if (timeLeft >= 0){
+        var timeRemaining = timeLeft;
+        var createPara = document.createElement("p");
+        createPara.textContent = "Your final score is: " + timeRemaining;
+        questionsDiv.appendChild(createPara);
+    }
+     
+
+ 
 }
 
 
